47d7f756fd3d1fb07161d4c298bfa4f601141756
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2023-10-03 22:40:36 +0300

    Do challenge
    Changed the myName variable to Lucija and the printed message

--- Assets/Scripts/GitClass.cs
+++ Assets/Scripts/GitClass.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class GitClass : MonoBehaviour
 {
-    string myName = "Git";
+    string myName = "Lucija";
     int myPower = 9001;
 
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello! My name is " + myName);
+        print("Hello World! My name is " + myName);
 
         if (myPower > 9000)
         {
