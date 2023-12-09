b6f424effb485c7c4ee4efe26272fd72c0aefcbd
AngelinaRozin <145436220+AngelinaRozin@users.noreply.github.com>
2023-12-09 12:43:22 +0200

    Changes with added picture and massege in welcome txt

--- Assets/Scripts/GitClass.cs
+++ Assets/Scripts/GitClass.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GitClass : MonoBehaviour
 {
-    string myName = "Slim Shady";
-    int myPower = 9001;
+    string myName = "Angelina";
+    int myPower = 31293;
 
     // Start is called before the first frame update
     void Start()
